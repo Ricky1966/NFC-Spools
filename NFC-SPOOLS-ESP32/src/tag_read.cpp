b5e6b59495e725cecbb58bb551b2a35a18eaad8c
Riccardo BRAMATI <riccardo.bramati@gmail.com>
2023-09-20 17:12:02 +0200

    Update tag_read.cpp

--- NFC-SPOOLS-ESP32/src/tag_read.cpp
+++ NFC-SPOOLS-ESP32/src/tag_read.cpp
@@ -1,32 +1,20 @@
-#include "tag_read.h"
+#include "tag_read.h"
 
-#define SS_PIN_1 4    // ESP32 pin GPIO4
-#define SS_PIN_2 5    // ESP32 pin GPIO5 
-#define SS_PIN_3 6    // ESP32 pin GPIO4
-#define SS_PIN_4 7    // ESP32 pin GPIO5
-#define MAX_SENSORS 4 //max number of sensors 
-#define RST_PIN 27    // ESP32 pin GPIO27
+extern MFRC522 mfrc522_1;
+extern MFRC522 mfrc522_2;
 
-extern MFRC522 mfrc522_1;//(SS_PIN_1, RST_PIN);
-extern MFRC522 mfrc522_2;//(SS_PIN_2, RST_PIN);
-
-extern NfcAdapter nfc_1;// = NfcAdapter(&mfrc522_1);
-extern NfcAdapter nfc_2;//= NfcAdapter(&mfrc522_2);
+extern NfcAdapter nfc_1;
+extern NfcAdapter nfc_2;
 
 extern char uid[30], tag_msg[100];
 extern String uid_str, mat_type, mat_color, spool_lenght, spool_weigth, temp_bed, temp_ext, t_fl_b, t_fl_e;
 bool tag_read(int sensor){
  
-  if (sensor == 1){
-    Serial.print("TAG.......");
-    Serial.print("Sensore :");
-    Serial.println(sensor);
+  if (sensor == 1){
     if (nfc_1.tagPresent()){
-      NfcTag tag = nfc_1.read();
-      Serial.print("UID: ");
+      NfcTag tag = nfc_1.read();
       uid_str = tag.getUidString(); 
-      uid_str.toCharArray(uid, uid_str.length() + 1);
-      Serial.println(uid_str);
+      uid_str.toCharArray(uid, uid_str.length() + 1);
       if (tag.hasNdefMessage()){ 
         NdefMessage message = tag.getNdefMessage();
         int recordCount = message.getRecordCount();
@@ -38,24 +26,18 @@ bool tag_read(int sensor){
           for (int c = 3; c < payloadLength; c++) {
             tag_msg_str += (char)payload[c];
           }
-          tag_msg_str.toCharArray(tag_msg, tag_msg_str.length() + 1);
-          //Serial.println(tag_msg_str);
+          tag_msg_str.toCharArray(tag_msg, tag_msg_str.length() + 1);
           loader(i,tag_msg_str);
         }
       }
     return true;
     }
   } 
-  if (sensor == 2){
-    Serial.print("TAG.......");
-    Serial.print("Sensore :");
-    Serial.println(sensor);
+  if (sensor == 2){
     if (nfc_2.tagPresent()){
-      NfcTag tag = nfc_2.read();
-      Serial.print("UID: ");
+      NfcTag tag = nfc_2.read();
       uid_str = tag.getUidString(); 
-      uid_str.toCharArray(uid, uid_str.length() + 1);
-      Serial.println(uid_str);
+      uid_str.toCharArray(uid, uid_str.length() + 1);
       if (tag.hasNdefMessage()){ 
         NdefMessage message = tag.getNdefMessage();
         int recordCount = message.getRecordCount();
@@ -67,8 +49,7 @@ bool tag_read(int sensor){
           for (int c = 3; c < payloadLength; c++) {
             tag_msg_str += (char)payload[c];
           }
-          tag_msg_str.toCharArray(tag_msg, tag_msg_str.length() + 1);
-          //Serial.println(tag_msg_str);
+          tag_msg_str.toCharArray(tag_msg, tag_msg_str.length() + 1);
           loader(i,tag_msg_str);
         }
       }
