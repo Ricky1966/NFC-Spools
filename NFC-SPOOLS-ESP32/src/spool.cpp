f6b8d62f3dd03ce6f5b36fd4a6f528f78280c334
Simone Gallina <simone@simonegallina.net>
2023-10-24 11:15:09 +0200

    Update spool.cpp

--- NFC-SPOOLS-ESP32/src/spool.cpp
+++ NFC-SPOOLS-ESP32/src/spool.cpp
@@ -22,7 +22,7 @@ Spool::Spool(){}
  */
 Spool::Spool(String uid_str)
 {
-    this->uid_str = uid_str;
+    uid_str = uid_str;
 }
 
 
@@ -31,7 +31,7 @@ Spool::Spool(String uid_str)
  */
 String Spool::get_spool_uid()
 {
-    return this->uid_str;
+    return uid_str;
 }
 
 
@@ -40,7 +40,7 @@ String Spool::get_spool_uid()
  */
 void Spool::put_mat_type(String mat_type)
 {
-    this->mat_type = mat_type;
+    mat_type = mat_type;
 }
 
 
@@ -49,7 +49,7 @@ void Spool::put_mat_type(String mat_type)
  */
 String Spool::get_mat_type()
 {
-    return this->mat_type;
+    return mat_type;
 }
 
 
@@ -58,7 +58,7 @@ String Spool::get_mat_type()
  */
 void Spool::put_mat_color(String mat_color)
 {
-    this->mat_color = mat_color;
+    mat_color = mat_color;
 }
 
 
@@ -67,7 +67,7 @@ void Spool::put_mat_color(String mat_color)
  */
 String Spool::get_mat_color()
 {
-    return this->mat_color;
+    return mat_color;
 }
 
 
@@ -76,7 +76,7 @@ String Spool::get_mat_color()
  */
 void Spool::put_lenght(String spool_lenght)
 {
-    this->spool_lenght = spool_lenght;
+    spool_lenght = spool_lenght;
 }
 
 
@@ -85,7 +85,7 @@ void Spool::put_lenght(String spool_lenght)
  */
 String Spool::get_lenght()
 {
-    return this->spool_lenght;
+    return spool_lenght;
 }
 
 
@@ -94,7 +94,7 @@ String Spool::get_lenght()
  */
 void Spool::put_weigth(String spool_weigth)
 {
-    this->spool_weigth = spool_weigth;
+    spool_weigth = spool_weigth;
 }
 
 
@@ -103,7 +103,7 @@ void Spool::put_weigth(String spool_weigth)
  */
 String Spool::get_weigth()
 {
-    return this->spool_weigth;
+    return spool_weigth;
 }
 
 
@@ -112,7 +112,7 @@ String Spool::get_weigth()
  */
 void Spool::put_temp_bed(String temp_bed)
 {
-    this->temp_bed = temp_bed;
+    temp_bed = temp_bed;
 }
 
 
@@ -121,7 +121,7 @@ void Spool::put_temp_bed(String temp_bed)
  */
 String Spool::get_temp_bed()
 {
-    return this->temp_bed;
+    return temp_bed;
 }
 
 
@@ -130,7 +130,7 @@ String Spool::get_temp_bed()
  */
 void Spool::put_temp_extruder(String temp_ext)
 {
-    this->temp_ext = temp_ext;
+    temp_ext = temp_ext;
 }
 
 
@@ -139,7 +139,7 @@ void Spool::put_temp_extruder(String temp_ext)
  */
 String Spool::get_temp_extruder()
 {
-    return this->temp_ext;
+    return temp_ext;
 }
 
 
@@ -148,7 +148,7 @@ String Spool::get_temp_extruder()
  */
 void Spool::put_bed_first_layer(String t_fl_b)
 {
-    this->t_fl_b = t_fl_b;
+    t_fl_b = t_fl_b;
 }
 
 
@@ -157,7 +157,7 @@ void Spool::put_bed_first_layer(String t_fl_b)
  */
 String Spool::get_bed_first_layer()
 {
-    return this->t_fl_b;
+    return t_fl_b;
 }
 
 
@@ -166,7 +166,7 @@ String Spool::get_bed_first_layer()
  */
 void Spool::put_ext_first_layer(String t_fl_e)
 {
-    this->t_fl_e = t_fl_e;
+    t_fl_e = t_fl_e;
 }
 
 
@@ -175,5 +175,5 @@ void Spool::put_ext_first_layer(String t_fl_e)
  */
 String Spool::get_ext_first_layer()
 {
-    return this->t_fl_e;
+    return t_fl_e;
 }
