9d420acc9dda4e627fc68572b906d6eca2177d48
Ricky1966 <riccardo.bramati@gmail.com>
2023-10-30 12:27:19 +0100

    new def_pages

---
+++ src/def_pages.cpp
@@ -0,0 +1,10 @@
+/**
+ * @brief   web server
+ *
+ * @author  Ricky1966
+ * @author  simonegallina (supervisor)
+ *
+ * @version 2.0
+ */
+
+#include "def_pages.h"
---
+++ src/def_pages.h
@@ -0,0 +1,21 @@
+/**
+ * @brief dEFINITIONS OF WEB PAGES
+ *
+ * @author Ricky1966
+ * @author simonegallina (supervisor)
+ *
+ * @version 2.0
+ */
+
+#ifndef _DEF_PAGES_H
+#define _DEF_PAGES_H
+
+/**
+ *
+ */
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+
+#endif
